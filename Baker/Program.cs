b627f250b4d534964775ebd8a4f36a93a4045b15
Debo <dwdeante0726@gmail.com>
2023-09-09 11:28:33 -0700

    Setup initial directory structure for project.

---
+++ Baker/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Baker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome! Here for the bread or the bread? (There also are pastries...)")
+    }
+  }
+}
