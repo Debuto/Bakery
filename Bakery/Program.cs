af31162298abb7c65a631be158a801254a649327
Deante Cacatian <dwdeante0726@gmail.com>
2023-09-14 12:10:36 -0700

    Correcting line 22 in Program.cs to Pastry Total Cost:

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,6 +9,7 @@ namespace Bakery
     {
       Console.WriteLine("Welcome! Here for the bread or the bread? (There also are pastries...)");
       Console.Write("How many loaves of Bread would you like to buy? Buy 2, get 1 free!!! ");
+      Console.Write("How many Pastries would you like to buy? Buy 3, get 1 free!!! ");
       int Quantity = int.Parse(Console.ReadLine());
       Bread bread = new Bread(Quantity);
       Pastry pastry = new Pastry(Quantity);
@@ -18,7 +19,7 @@ namespace Bakery
       pastry.CalculateTotalCost();
       // Console.WriteLine($"Total Cost: ${bread.TotalCost}");
         Console.WriteLine($"Bread Total Cost: ${bread.TotalCost}");
-        Console.WriteLine($"Bread Total Cost: ${pastry.TotalCost}");
+        Console.WriteLine($"Pastry Total Cost: ${pastry.TotalCost}");
     }
   }
 }
