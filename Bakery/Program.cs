579d4bb018672ae6f70a21e7f4f7bc70756e0e78
Deante Cacatian <dwdeante0726@gmail.com>
2023-09-14 13:51:45 -0700

    Adjusting prompt and UI in Program.cs with if statements to decide if user wants to specify their order between 3 options.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,18 +8,30 @@ namespace Bakery
     static void Main(string[] args)
     {
       Console.WriteLine("Welcome! Here for the bread or the bread? (There also are pastries...)");
-      Console.Write("How many loaves of Bread would you like to buy? Buy 2, get 1 free!!! ");
-      Console.Write("How many Pastries would you like to buy? Buy 3, get 1 free!!! ");
-      int Quantity = int.Parse(Console.ReadLine());
-      Bread bread = new Bread(Quantity);
-      Pastry pastry = new Pastry(Quantity);
-      Console.WriteLine($"Bread Price: ${bread.UnitCost} each");
-      Console.WriteLine($"Pastry Price: ${pastry.UnitCost} each");
-      bread.CalculateTotalCost();
-      pastry.CalculateTotalCost();
-      // Console.WriteLine($"Total Cost: ${bread.TotalCost}");
-        Console.WriteLine($"Bread Total Cost: ${bread.TotalCost}");
-        Console.WriteLine($"Pastry Total Cost: ${pastry.TotalCost}");
+      Console.Write("Please choose what you would like to order! (bread/pastry/both): ");
+      string choice = Console.ReadLine().ToLower();
+
+      if (choice == "bread" || choice == "both")
+      {
+        Console.Write("How many loaves of Bread would you like to buy? Buy 2, get 1 free!!! ");
+        int Quantity = int.Parse(Console.ReadLine());
+        Bread bread = new Bread(Quantity);
+        Console.WriteLine($"Bread Price: ${bread.UnitCost} each");
+        bread.CalculateTotalCost();
+        if (Quantity > 0)
+          Console.WriteLine($"Bread Total Cost: ${bread.TotalCost}");
+      }
+
+      if (choice == "pastry" || choice == "both")
+      {
+        Console.Write("How many Pastries would you like to buy? Buy 3, get 1 free!!! ");
+        int Quantity = int.Parse(Console.ReadLine());
+        Pastry pastry = new Pastry(Quantity);
+        Console.WriteLine($"Pastry Price: ${pastry.UnitCost} each");      
+        pastry.CalculateTotalCost();
+        if (Quantity > 0)
+          Console.WriteLine($"Pastry Total Cost: ${pastry.TotalCost}");
+      } 
     }
   }
 }
